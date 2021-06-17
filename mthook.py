d327b92816320de91421ba3bb99506144536e60d
igolaizola <11333576+igolaizola@users.noreply.github.com>
2021-06-17 16:27:56 +0200

    Change telethon to pyrogram

--- mthook.py
+++ mthook.py
@@ -10,66 +10,61 @@ import requests
 import subprocess
 import sys
 from datetime import datetime
-from telethon import TelegramClient,events
+from pyrogram import Client, filters
 
-async def main():
-    await client.start()
+def handle(fs, message):
+    id = ''
+    if hasattr(message, 'chat') and hasattr(message.chat, 'username'):
+        id = message.chat.username
+    elif hasattr(message, 'chat') and hasattr(message.chat, 'id'):
+        id = message.chat.id
+    else:
+        return
+    
+    if not id in fs:
+        return
 
-    filters = {}
+    filter = fs[id]
+    match = filter['regex'].search(message.text)
+    if match is None:
+        return
 
-    for arg in sys.argv[4:]:
-        vals = arg.split(',')
-        reg = re.compile(vals[2])
-        filters[vals[0]] = {'hook': vals[1], 'regex': reg}
-
-    @client.on(events.NewMessage(pattern='.+'))
-    async def handler(event):
-        if not hasattr(event, 'message'):
-            return
-        id = ''
-        if hasattr(event.message.peer_id, 'channel_id'):
-            id = str(event.message.peer_id.channel_id)
-        elif hasattr(event.message.peer_id, 'user_id'):
-            id = str(event.message.peer_id.user_id)
-        elif hasattr(event.message.peer_id, 'chat_id'):
-            id = str(event.message.peer_id.chat_id)
-        else:
-            return
-
-        if not id in filters:
-            return
+    hook = filter['hook']
+    if hook.startswith('http'):
+        args = []
+        for i in range (len(match.groups())):
+            args.append(match.group(i+1))
+        url = filter['hook']+','.join(args)
+        print(datetime.now().time(), url)
+        requests.get(url)
+    else:
+        args = [filter['hook']]
+        for i in range (len(match.groups())):
+            args.append(match.group(i+1))
         
-        filter = filters[id]
-        match = filter['regex'].search(event.message.message)
-        if match is None:
-            return
-
-        hook = filter['hook']
-        if hook.startswith('http'):
-            args = []
-            for i in range (len(match.groups())):
-                args.append(match.group(i+1))
-            url = filter['hook']+','.join(args)
-            print(datetime.now().time(), url)
-            requests.get(url)
-        else:
-            args = [filter['hook']]
-            for i in range (len(match.groups())):
-                args.append(match.group(i+1))
-            
-            print(datetime.now().time(),' '.join(args))
-            subprocess.run(args)
+        print(datetime.now().time(),' '.join(args))
+        subprocess.run(args)
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
-    print('1.21.1')
+    print('1.2.9')
 elif len(sys.argv) < 5:
     print('Usage mthook.py api_id api_hash session "chat_id,command,regex" "chat_id,command,regex" ...')
 else:
     # values from my.telegram.org
     api_id = sys.argv[1]
     api_hash = sys.argv[2]
-    client = TelegramClient(sys.argv[3], api_id, api_hash)
-    with client:
-        client.loop.run_until_complete(main())
-        print(datetime.now().time(), 'mthook started')
-        client.run_until_disconnected()
+    
+    app = Client(sys.argv[3], api_id, api_hash)
+    print(datetime.now().time(), 'mthook started')
+
+    fs = {}
+    for arg in sys.argv[4:]:
+        vals = arg.split(',')
+        reg = re.compile(vals[2])
+        fs[vals[0]] = {'hook': vals[1], 'regex': reg}
+
+    @app.on_message(filters.text)
+    def onMessage(client, message):
+        handle(fs, message)
+
+    app.run()
